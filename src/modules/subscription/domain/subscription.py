b0f21548b6b2cfce883ccf7536967e35921f5c20
Xander44-4 <xandercruz2k@gmail.com>
2025-07-09 01:37:43 -0400

    feature(subscription) : add a subscrition model

--- src/modules/subscription/domain/subscription.py
+++ src/modules/subscription/domain/subscription.py
@@ -1,1 +1,17 @@
-# Here its gonna be the user model, in our case we're going to use pydantic.
+from datetime import datetime
+from typing import List
+from pydantic import BaseModel, Field
+
+from src.lib.py_object_id import PyObjectId
+
+
+class Subscription(BaseModel):
+    id : PyObjectId
+    user_id : PyObjectId
+    stripe_customer_id : str
+    stripe_subscription_id : str
+    status : List[int] = Field(ge=1, le=3)
+    plan : List[int] = Field(ge=1, le=2)
+    start_date: datetime
+    end_date: datetime
+    update_at: datetime
