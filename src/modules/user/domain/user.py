d6056f5dd3a26be41461d693ab09acc117991fa4
enmanuemartex <enmanuelmartex@gmail.com>
2025-07-09 00:52:25 -0400

    Feature: Add a user model

--- src/modules/user/domain/user.py
+++ src/modules/user/domain/user.py
@@ -1,13 +1,15 @@
 from pydantic import BaseModel,Field,EmailStr
 from typing import Optional
+from src.lib.py_object_id import PyObjectId
+
 
 class User(BaseModel):
-    id: int
+    id: PyObjectId
     username: str = Field(...,min_length=3, max_length=30,pattern=r"^[a-zA-Z0-9_]+$")
-    display_name: str = Field(..., min_length=3,max_length=255)
+    display_name: str = Field(..., min_length=3,max_length=40)
     email: EmailStr
-    hashed_password: str
-    phone: str = Field(pattern=r"^\+\d{2} \(\d{3}\) \d{3}-\d{4}$")
-    accent_color: int = Field(ge=0,le=16777215)
-    flags: int
     address: Optional[str] = Field(None, max_length=255)
+    phone: str = Field(pattern=r"^\+\d{2} \(\d{3}\) \d{3}-\d{4}$")
+    accent_color: int = Field(ge=0, le=16777215)
+    flags: int
+    hashed_password: str
