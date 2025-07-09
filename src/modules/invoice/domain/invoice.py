f3d71b4c2d4e5ed6a021390f2e4fa7ab69fd9377
enmanuemartex <enmanuelmartex@gmail.com>
2025-07-09 01:12:03 -0400

    Feature: Add an invoice model

--- src/modules/invoice/domain/invoice.py
+++ src/modules/invoice/domain/invoice.py
@@ -1,1 +1,11 @@
-# Here its gonna be the user model, in our case we're going to use pydantic.
+from datetime import datetime
+from pydantic import BaseModel, Field
+from src.lib.py_object_id import PyObjectId
+
+class Invoice(BaseModel):
+    id: PyObjectId
+    client_id: PyObjectId
+    date: datetime
+    services: list[PyObjectId]
+    notes: str = Field(min_length=10, max_length=1000)
+    total: float
