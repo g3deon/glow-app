c1f3aca6331b22a4537d3914af499936ceb75fef
enmanuemartex <enmanuelmartex@gmail.com>
2025-07-09 01:19:25 -0400

    refactor(Payment_history): edit fields that were wrong

--- src/modules/payment_history/domain/payment_history.py
+++ src/modules/payment_history/domain/payment_history.py
@@ -2,13 +2,10 @@ from datetime import datetime
 from pydantic import BaseModel, Field
 from src.lib.py_object_id import PyObjectId
 
-
-# Here its gonna be the user model, in our case we're going to use pydantic.
-
 class PaymentHistory(BaseModel):
     id: PyObjectId
-    invoid_id : PyObjectId
-    amout: float
-    method: int = Field(ge=1, le=3) #1-tarjeta 2-efectivo 3-etc
+    invoice_id : PyObjectId
+    amount: float
+    method: int = Field(ge=1, le=3)
     payment_date: datetime
-    status: int = Field(ge=1, le=3) # 1-pagado, 2-pendiente, 3- fallido
+    status: int = Field(ge=1, le=3)
