e29d7ebdc5c4f8427a757683ec6b1118633f71eb
enmanuemartex <enmanuelmartex@gmail.com>
2025-07-09 01:44:17 -0400

    Refactor(invoice): edit some fields

--- src/modules/payment_history/domain/payment_history.py
+++ src/modules/payment_history/domain/payment_history.py
@@ -5,7 +5,7 @@ from src.lib.py_object_id import PyObjectId
 class PaymentHistory(BaseModel):
     id: PyObjectId
     invoice_id : PyObjectId
-    amount: float
+    amount: float = Field(..., ge=0)
     method: int = Field(ge=1, le=3)
     payment_date: datetime
     status: int = Field(ge=1, le=3)
