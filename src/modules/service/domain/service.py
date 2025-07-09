1158d101f6cf8b476457aaff1339fb3f28e1efa8
enmanuemartex <enmanuelmartex@gmail.com>
2025-07-09 01:42:26 -0400

    feature(Service): Add a service model

--- src/modules/service/domain/service.py
+++ src/modules/service/domain/service.py
@@ -1,1 +1,12 @@
-# Here its gonna be the user model, in our case we're going to use pydantic.
+from pydantic import BaseModel, Field
+
+from src.lib.py_object_id import PyObjectId
+
+
+class Service(BaseModel):
+    id: PyObjectId
+    name: str = Field(...,min_length=3, max_length=30)
+    description: str = Field(min_length=10, max_length=1000)
+    price: float = Field(..., ge=0)
+    category_id: PyObjectId
+    accent_color: int = Field(...,ge=0, le=16777215)
