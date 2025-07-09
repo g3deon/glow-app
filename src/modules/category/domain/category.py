d15b4544968e30978a1f657d3f892d24daa373e3
enmanuemartex <enmanuelmartex@gmail.com>
2025-07-09 01:54:56 -0400

    Feature(Category): Add a category module and model

---
+++ src/modules/category/domain/category.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel, Field
+from src.lib.py_object_id import PyObjectId
+
+class Category(BaseModel):
+    id: PyObjectId
+    name: str = Field(...,min_length=3, max_length=30)
+    description: str = Field(min_length=10, max_length=1000)
+    accent_color: int = Field(ge=0, le=16777215)
