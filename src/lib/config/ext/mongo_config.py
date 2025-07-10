e72a59200b42776a3ec0225ab69ce426af46733a
Xander44-4 <xandercruz2k@gmail.com>
2025-07-10 01:36:42 -0400

    feature(mongoDB) : add a Mongo class to connect and close the connection using the lib PyMongo Async API

---
+++ src/lib/config/ext/mongo_config.py
@@ -0,0 +1,27 @@
+import asyncio
+
+from pymongo import AsyncMongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+MONGO_URI = os.getenv("MONGO_URI")
+MONGO_DB = os.getenv("MONGO_DATABASE")
+
+class MongoDB:
+    def __init__(self):
+        if not MONGO_URI:
+            raise Exception('Mongo URI not set')
+        self.client = AsyncMongoClient(MONGO_URI)
+        self.database = self.client[MONGO_DB]
+
+    async def connect(self):
+        return await self.client.aconnect()
+
+    async def close(self):
+        if self.client:
+            await self.client.close()
+
+    async def insert(self, colletion : str , inserted_data : dict[str,any]):
+        await self.database[colletion].insert_one(inserted_data)
+
