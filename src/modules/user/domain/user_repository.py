cab3305b0179130416390b84ea337c8423487b16
Xander44-4 <xandercruz2k@gmail.com>
2025-07-10 10:17:45 -0400

    feature(user_repository) : add the abstract methods of user_repository in domain to use in infrastructure

--- src/modules/user/domain/user_repository.py
+++ src/modules/user/domain/user_repository.py
@@ -1,4 +1,32 @@
-# Here its gonna be the interface of user, bascally the funtions that you're gonna have
-# los metodos que usaremops con la base de datos
+from abc import ABC, abstractmethod
 
-#abtracciones
+from src.modules.user.domain.user import User
+
+
+class UserRepository(ABC):
+
+    @abstractmethod
+    async def create_user(self, user: User) -> User:
+        pass
+
+    @abstractmethod
+    async def get_all(self):
+        pass
+
+    @abstractmethod
+    async def get_user_by_id(self, user_id: str) -> User:
+        pass
+
+    @abstractmethod
+    async def get_user_by_username(self, username: str) -> User:
+        pass
+
+    @abstractmethod
+    async def get_user_by_email(self, email: str) -> User:
+        pass
+
+    @abstractmethod
+    async def delete(self, user_id: str) -> bool:
+        pass
+
+
