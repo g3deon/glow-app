8296df0dde0959ee1e26bfaabaa4a80e7660e9dd
Xander44-4 <xandercruz2k@gmail.com>
2025-07-10 10:28:03 -0400

    feat(user_service.impl): add UserService logic from user_repository interface

--- src/modules/user/application/user_service_impl.py
+++ src/modules/user/application/user_service_impl.py
@@ -1,1 +1,31 @@
-#aqui van las implementaciones
+from src.modules.user.domain.user import User
+from src.modules.user.domain.user_repository import UserRepository
+
+
+class UserService:
+    def __init__(self, user_repository : UserRepository):
+        self.user_repository = user_repository
+
+    async def create_user(self, user: User) -> User:
+        return await self.user_repository.create_user(user)
+
+
+    async def get_all(self):
+        return await self.user_repository.get_all()
+
+
+    async def get_user_by_id(self, user_id: str) -> User:
+        return await self.user_repository.get_user_by_id(user_id)
+
+
+    async def get_user_by_username(self, username: str) -> User:
+        return await self.user_repository.get_user_by_username(username)
+
+
+    async def get_user_by_email(self, email: str) -> User:
+        return await self.user_repository.get_user_by_email(email)
+
+
+    async def delete(self, user_id: str) -> bool:
+        return await self.user_repository.delete(user_id)
+
