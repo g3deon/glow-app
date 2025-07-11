68e4c47e0d6716ba2b6f8ec6d9fcac6289dfa8e7
enmanuemartex <enmanuelmartex@gmail.com>
2025-07-11 02:11:32 -0400

    Feature(stats_history): Add stats_history model

--- src/modules/stats_history/domain/stats_history.py
+++ src/modules/stats_history/domain/stats_history.py
@@ -1,1 +1,18 @@
-# Here its gonna be the user model, in our case we're going to use pydantic.
+from datetime import date
+from pydantic import BaseModel, Field
+from src.lib.py_object_id import PyObjectId
+
+
+class StatsHistory(BaseModel):
+    id: PyObjectId
+    user_id: PyObjectId
+    total_invoiced_amount: float =Field(ge=0)
+    total_paid_amount: float = Field(ge=0)
+    total_unpaid_amount: float
+    total_invoices_count: int = Field(ge=0)
+    pending_invoices_count: int = Field(ge=0)
+    completed_invoices_count: int = Field(ge=0)
+    total_clients: int = Field(ge=0)
+    start_date: date
+    end_date: date
+
