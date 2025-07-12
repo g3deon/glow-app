cb398c5f81a2bd9b02df88b2551c139290aa6844
enmanuemartex <enmanuelmartex@gmail.com>
2025-07-12 01:19:01 -0400

    Feature(Client_repository): Create client repository

--- src/modules/client/domain/client_repository.py
+++ src/modules/client/domain/client_repository.py
@@ -1,4 +1,25 @@
-# Here its gonna be the interface of user, bascally the funtions that you're gonna have
-# los metodos que usaremops con la base de datos
+from abc import ABC, abstractmethod
+from typing import List,Optional
+from src.lib.py_object_id import PyObjectId
+from src.modules.client.domain.client import Client
 
-#abtracciones
+class ClientRepository(ABC):
+    @abstractmethod
+    def get_all(self) -> List[Client]:
+        pass
+
+    @abstractmethod
+    def get_by_id(self, id:PyObjectId) -> Optional[Client]:
+        pass
+
+    @abstractmethod
+    def create(self, client:Client) -> Client:
+        pass
+
+    @abstractmethod
+    def update(self, id: PyObjectId, client:Client, update_field: list[str]) -> Optional[Client]:
+        pass
+
+    @abstractmethod
+    def delete(self, id:PyObjectId):
+        pass
