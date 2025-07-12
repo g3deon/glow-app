5c5402f91702e88b65c81a5bd8ccdb96dd877fe4
enmanuemartex <enmanuelmartex@gmail.com>
2025-07-12 02:45:50 -0400

    Feature(Category_repository): Add category_repository

--- src/modules/category/domain/category_repository.py
+++ src/modules/category/domain/category_repository.py
@@ -1,4 +1,26 @@
-# Here its gonna be the interface of user, bascally the funtions that you're gonna have
-# los metodos que usaremops con la base de datos
+from abc import ABC,abstractmethod
+from typing import List, Optional
+from src.lib.py_object_id import PyObjectId
+from src.modules.category.domain.category import Category
 
-#abtracciones
+
+class CategoryRepository(ABC):
+    @abstractmethod
+    def get_all(self) -> List[Category]:
+        pass
+
+    @abstractmethod
+    def get_by_id(self, identification: PyObjectId) -> Optional[Category]:
+        pass
+
+    @abstractmethod
+    def create(self, category: Category) -> Category:
+        pass
+
+    @abstractmethod
+    def update(self, identification: PyObjectId, category: Category, update_field: list[str]) -> Optional[Category]:
+        pass
+
+    @abstractmethod
+    def delete(self, identification: PyObjectId) -> bool:
+        pass
