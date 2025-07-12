3f4f9ff9b261b2b45eb8eae3f82f2d490bbc5c3e
enmanuemartex <enmanuelmartex@gmail.com>
2025-07-12 02:46:29 -0400

    Feature(Category_Service_Impl): Add Category_Service_Impl

--- src/modules/category/application/category_service_impl.py
+++ src/modules/category/application/category_service_impl.py
@@ -1,1 +1,24 @@
-#aqui van las implementaciones
+from typing import Optional
+
+from src.lib.py_object_id import PyObjectId
+from src.modules.category.domain.category_repository import CategoryRepository
+from src.modules.category.domain.category import Category
+
+class CategoryServiceImplementation:
+    def __init__(self, category_repository: CategoryRepository):
+        self.category_repository = category_repository
+
+    def get_all(self) -> list[Category]:
+        return self.category_repository.get_all()
+
+    def get_by_id(self, identification: PyObjectId) -> Optional[Category]:
+        return self.category_repository.get_by_id(identification)
+
+    def create(self, category: Category) -> Category:
+        return self.category_repository.create(category)
+
+    def update(self, identification: PyObjectId, category: Category, update_field: list[str]) -> Category:
+        return self.category_repository.update(identification,category,update_field)
+
+    def delete(self, identification: PyObjectId) -> bool:
+        return self.category_repository.delete(identification)
