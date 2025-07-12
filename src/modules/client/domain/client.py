8961c046cab2d22d3e8f2da5b01242dc0c11d068
enmanuemartex <enmanuelmartex@gmail.com>
2025-07-12 00:23:57 -0400

    Refactor(Client): Add some fields

--- src/modules/client/domain/client.py
+++ src/modules/client/domain/client.py
@@ -1,9 +1,8 @@
 from pydantic import BaseModel, EmailStr, Field
 from src.lib.py_object_id import PyObjectId
 
-# Here its gonna be the user model, in our case we're going to use pydantic.
-
 class Client(BaseModel):
     id: PyObjectId
+    name : str = Field(...,min_length=3, max_length=30)
     email : EmailStr
     phone : str = Field(pattern=r"^\+\d{2} \(\d{3}\) \d{3}-\d{4}$")
