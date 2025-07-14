e6808d0224395dea736723f2d4be315123acb73a
Xander44-4 <xandercruz2k@gmail.com>
2025-07-13 20:26:09 -0400

    refactor(main): eliminate shutdown and up functions of the fastapi

--- src/main.py
+++ src/main.py
@@ -1,17 +1,9 @@
 from fastapi import FastAPI
 
-from src.lib.config.ext.mongo_config import mongo_instance
+
 from src.modules.user.infrastructure.user_fastapi_router import user_routes
 
 app = FastAPI(title='Glow App')
 
 app.include_router(user_routes)
 
-
-@app.on_event("startup")
-async def startup_event():
-    await mongo_instance.connect()
-
-@app.on_event("shutdown")
-async def shutdown_event():
-    await mongo_instance.close()
