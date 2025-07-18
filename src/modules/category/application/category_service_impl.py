8f74e170c4d69b1ebd549a061d1de93bb46c4041
enmanuemartex <enmanuelmartex@gmail.com>
2025-07-18 01:26:10 -0400

    Refactor(category_service_impl): changing the function return expectation to a coroutine

--- src/modules/category/application/category_service_impl.py
+++ src/modules/category/application/category_service_impl.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, Any, Coroutine
 
 from src.lib.py_object_id import PyObjectId
 from src.modules.category.domain.category_repository import CategoryRepository
@@ -8,17 +8,18 @@ class CategoryServiceImplementation:
     def __init__(self, category_repository: CategoryRepository):
         self.category_repository = category_repository
 
-    def get_all(self) -> list[Category]:
+    def get_all(self) -> Coroutine[Any, Any, list[Category]]:
         return self.category_repository.get_all()
 
-    def get_by_id(self, identification: PyObjectId) -> Optional[Category]:
+    def get_by_id(self, identification: PyObjectId) -> Coroutine[Any, Any, Category | None]:
         return self.category_repository.get_by_id(identification)
 
-    def create(self, category: Category) -> Category:
+    def create(self, category: Category) -> Coroutine[Any, Any, Category]:
         return self.category_repository.create(category)
 
-    def update(self, identification: PyObjectId, category: Category, update_field: list[str]) -> Category:
+    def update(self, identification: PyObjectId, category: Category, update_field: list[str]) -> Coroutine[
+        Any, Any, Category | None]:
         return self.category_repository.update(identification,category,update_field)
 
-    def delete(self, identification: PyObjectId) -> bool:
+    def delete(self, identification: PyObjectId) -> Coroutine[Any, Any, bool]:
         return self.category_repository.delete(identification)
