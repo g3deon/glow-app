c63fbdf789155ec7fad109b43f490714b80ed835
enmanuemartex <enmanuelmartex@gmail.com>
2025-07-18 01:27:01 -0400

    Refactor(category_repository): changing the function return expectation to a coroutine

--- src/modules/category/domain/category_repository.py
+++ src/modules/category/domain/category_repository.py
@@ -6,21 +6,21 @@ from src.modules.category.domain.category import Category
 
 class CategoryRepository(ABC):
     @abstractmethod
-    def get_all(self) -> List[Category]:
+    async def get_all(self) -> List[Category]:
         pass
 
     @abstractmethod
-    def get_by_id(self, identification: PyObjectId) -> Optional[Category]:
+    async def get_by_id(self, identification: PyObjectId) -> Optional[Category]:
         pass
 
     @abstractmethod
-    def create(self, category: Category) -> Category:
+    async def create(self, category: Category) -> Category:
         pass
 
     @abstractmethod
-    def update(self, identification: PyObjectId, category: Category, update_field: list[str]) -> Optional[Category]:
+    async def update(self, identification: PyObjectId, category: Category, update_field: list[str]) -> Optional[Category]:
         pass
 
     @abstractmethod
-    def delete(self, identification: PyObjectId) -> bool:
+    async def delete(self, identification: PyObjectId) -> bool:
         pass
