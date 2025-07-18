45be892a79e0ee3a84a9abaf6ea54a6de3b0acb5
Xander44-4 <xandercruz2k@gmail.com>
2025-07-18 00:19:17 -0400

    fix(main): fix some errors in main

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from src.modules.auth.infrastructure.auth_fastapi_router import  auth_routes
+from src.modules.client.infrastructure.client_fastapi_router import client_routes
 from src.modules.user.infrastructure.user_fastapi_router import user_routes
 
 app = FastAPI(title='Glow App')
@@ -7,3 +8,5 @@ app = FastAPI(title='Glow App')
 app.include_router(user_routes)
 
 app.include_router(auth_routes)
+
+app.include_router(client_routes)
