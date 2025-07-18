6e2e84859b823429bb972c05d4a1f51947f9874c
enmanuemartex <enmanuelmartex@gmail.com>
2025-07-18 01:18:56 -0400

    Feature(service_repository_impl): Add the service repository implementation

--- src/modules/service/application/service_service_impl.py
+++ src/modules/service/application/service_service_impl.py
@@ -1,1 +1,28 @@
-#aqui van las implementaciones
+from typing import Optional, Any, Coroutine
+
+from src.lib.py_object_id import PyObjectId
+from src.modules.service.domain.service import Service
+from src.modules.service.domain.service_repository import Service_Repository
+
+
+class ServiceImplementation:
+    def __init__(self, service_repository: Service_Repository):
+        self.service_repository = service_repository
+
+    def get_all(self):
+        return self.service_repository.get_all()
+
+    def get_by_id(self, identification: PyObjectId):
+        return self.service_repository.get_by_id(identification)
+
+    def create(self, service: Service):
+        return self.service_repository.create(service)
+
+    def update(self, identification: PyObjectId, service: Service, update_field: list[str]) -> Coroutine[
+        Any, Any, Service | None]:
+        return self.service_repository.update(identification,service,update_field)
+
+    def delete(self, identification: PyObjectId):
+        return self.service_repository.delete(identification)
+
+
