2f03a304c99ee44b3d3ebfc3fbb2a32a63d4b632
enmanuemartex <enmanuelmartex@gmail.com>
2025-07-18 01:18:30 -0400

    Feature(service_repository): Add the service repository

--- src/modules/service/domain/service_repository.py
+++ src/modules/service/domain/service_repository.py
@@ -1,4 +1,27 @@
-# Here its gonna be the interface of user, bascally the funtions that you're gonna have
-# los metodos que usaremops con la base de datos
+from abc import ABC, abstractmethod
+from typing import Optional,List
+from src.lib.py_object_id import PyObjectId
+from src.modules.service.domain.service import Service
 
-#abtracciones
+class Service_Repository(ABC):
+
+    @abstractmethod
+    async def get_all(self) -> List[Service]:
+        pass
+
+    @abstractmethod
+    async def get_by_id(self, identification: PyObjectId) -> Optional[Service]:
+        pass
+
+    @abstractmethod
+    async def create(self, service: Service) -> Service:
+        pass
+
+    @abstractmethod
+    async def update(self, identification: PyObjectId, service:Service, update_field: list[str]) -> Optional[Service]:
+        pass
+
+    @abstractmethod
+    async def delete(self, identification: PyObjectId) -> bool:
+        pass
+
