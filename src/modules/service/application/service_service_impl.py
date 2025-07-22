84f05d342e3e61080f425d253c2ac2ba6dfeea07
enmanuemartex <enmanuelmartex@gmail.com>
2025-07-22 02:37:27 -0400

    Refactor(service_service_impl): Change Class Name to the new one

--- src/modules/service/application/service_service_impl.py
+++ src/modules/service/application/service_service_impl.py
@@ -2,27 +2,26 @@ from typing import Optional, Any, Coroutine
 
 from src.lib.py_object_id import PyObjectId
 from src.modules.service.domain.service import Service
-from src.modules.service.domain.service_repository import Service_Repository
+from src.modules.service.domain.service_repository import  ServiceRepository
 
 
 class ServiceImplementation:
-    def __init__(self, service_repository: Service_Repository):
+    def __init__(self, service_repository: ServiceRepository):
         self.service_repository = service_repository
 
-    def get_all(self):
-        return self.service_repository.get_all()
+    async def get_all(self):
+        return await self.service_repository.get_all()
 
-    def get_by_id(self, identification: PyObjectId):
-        return self.service_repository.get_by_id(identification)
+    async def get_by_id(self, identification: PyObjectId):
+        return await self.service_repository.get_by_id(identification)
 
-    def create(self, service: Service):
-        return self.service_repository.create(service)
+    async def create(self, service: Service):
+        return await self.service_repository.create(service)
 
-    def update(self, identification: PyObjectId, service: Service, update_field: list[str]) -> Coroutine[
-        Any, Any, Service | None]:
-        return self.service_repository.update(identification,service,update_field)
+    async def update(self, identification: PyObjectId, service: Service, update_field: list[str]) -> Service:
+        return await self.service_repository.update(identification,service,update_field)
 
-    def delete(self, identification: PyObjectId):
-        return self.service_repository.delete(identification)
+    async def delete(self, identification: PyObjectId):
+        return await self.service_repository.delete(identification)
 
 
