73986a709789eccc6d485f3468efb6961768cb02
enmanuemartex <enmanuelmartex@gmail.com>
2025-07-23 03:04:03 -0400

    Feature(shop_repository): Adding Shop Repository

--- src/modules/shop/domain/shop_repository.py
+++ src/modules/shop/domain/shop_repository.py
@@ -1,4 +1,26 @@
-# Here its gonna be the interface of user, bascally the funtions that you're gonna have
-# los metodos que usaremops con la base de datos
+from abc import ABC,abstractmethod
+from typing import List, Optional
+from src.lib.py_object_id import PyObjectId
+from src.modules.shop.domain.shop import Shop
 
-#abtracciones
+
+class ShopRepository(ABC):
+    @abstractmethod
+    async def get_all(self) -> List[Shop]:
+        pass
+
+    @abstractmethod
+    async def get_by_id(self, identification: PyObjectId) -> Optional[Shop]:
+        pass
+
+    @abstractmethod
+    async def create(self, shop: Shop) -> Shop:
+        pass
+
+    @abstractmethod
+    async def update(self, identification: PyObjectId, shop: Shop, update_field: list[str]) -> Optional[Shop]:
+        pass
+
+    @abstractmethod
+    async def delete(self, identification: PyObjectId) -> bool:
+        pass
