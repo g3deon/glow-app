c72cfdd6af8cd99547774d8340fd1c97547906d2
enmanuemartex <enmanuelmartex@gmail.com>
2025-07-23 03:10:19 -0400

    Feature(stats_history_repository): building stats history repository

--- src/modules/stats_history/domain/stats_history_repository.py
+++ src/modules/stats_history/domain/stats_history_repository.py
@@ -1,4 +1,30 @@
-# Here its gonna be the interface of user, bascally the funtions that you're gonna have
-# los metodos que usaremops con la base de datos
+from abc import ABC, abstractmethod
+from datetime import datetime
+from typing import List,Optional
+from src.lib.py_object_id import PyObjectId
+from src.modules.stats_history.domain.stats_history import StatsHistory
 
-#abtracciones
+class StatsHistoryRepository(ABC):
+    @abstractmethod
+    async def get_all(self) -> List[StatsHistory]:
+        pass
+
+    @abstractmethod
+    async def get_by_period(self, identification: PyObjectId, start_date: datetime, end_date: datetime) -> Optional[StatsHistory]:
+        pass
+
+    @abstractmethod
+    async def get_by_id(self, identification: PyObjectId) -> Optional[StatsHistory]:
+        pass
+
+    @abstractmethod
+    async def create(self, history: StatsHistory) -> StatsHistory:
+        pass
+
+    @abstractmethod
+    async def update(self, identification: PyObjectId, history: StatsHistory, update_field:list[str]) -> Optional[StatsHistory]:
+        pass
+
+    @abstractmethod
+    async def delete(self, identification: PyObjectId) -> bool:
+        pass
