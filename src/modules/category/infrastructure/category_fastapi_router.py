2cbc3f04586b6e2a7d8d986b364ccced247fec19
Xander44-4 <xandercruz2k@gmail.com>
2025-07-23 17:38:42 -0400

    refactor(category_delete_route):Add a response 204 when the DELETE function executes correctly.

--- src/modules/category/infrastructure/category_fastapi_router.py
+++ src/modules/category/infrastructure/category_fastapi_router.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter, HTTPException
+from starlette import status
+
 from src.modules.category.application.category_service_impl import CategoryServiceImpl
 from src.modules.category.domain.category import Category
 from src.modules.category.infrastructure.category_mongo_repository import CategoryMongoRepository
@@ -7,7 +9,7 @@ from src.modules.category.infrastructure.category_mongo_repository import Catego
 
 class HttpCategoryRouter:
     def __init__(self):
-        self.category = CategoryServiceImpl(CategoryMongoRepository())
+        self.service = CategoryServiceImpl(CategoryMongoRepository())
         self.router = APIRouter(
             prefix="/categories",
             tags=["Category"],
@@ -20,39 +22,45 @@ class HttpCategoryRouter:
         @self.router.get('/')
         async def get_all():
             try:
-                return await self.category.get_all()
+                return await self.service.get_all()
             except HTTPException as e:
                 raise HTTPException(status_code=404) from e
 
         @self.router.get('/{id}')
         async def get_by_id(_id):
             try:
-                return await self.category.get_by_id(_id)
+                return await self.service.get_by_id(_id)
             except HTTPException as e:
                 raise HTTPException(status_code=404) from e
 
         @self.router.post('/', status_code=201)
         async def create(category: Category):
             try:
-                return await self.category.create(category)
+                return await self.service.create(category)
             except HTTPException as e:
                 raise HTTPException(status_code=400) from e
 
-        @self.router.put('/{id}')
-        async def update(category: Category, _id, update_fields: list[str]):
+        @self.router.put('/update/{id}')
+        async def update(category: Category, _id , update_fields: list[str]):
             try:
-                return await self.category.update(_id, category, update_fields)
+                return await self.service.update(_id, category, update_fields)
             except HTTPException as e:
                 raise HTTPException(status_code=404) from e
+            except Exception as e:
+                raise HTTPException(status_code=400) from e
 
         @self.router.delete('/{id}')
         async def delete(_id):
             try:
-                return await self.category.delete(_id)
+                deleted = await self.service.delete(_id)
+                if deleted:
+                    return status.HTTP_204_NO_CONTENT
             except HTTPException as e:
+                raise HTTPException(status_code=404) from e
+            except Exception as e:
                 raise HTTPException(status_code=404) from e
 
 categories_routes = HttpCategoryRouter().router
 
-#todo solve how to use the update and return something when you delete
+
 
