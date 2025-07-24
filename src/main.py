4aeab042d0b8eab4f887a1253ca0f9a9e16409b3
Xander44-4 <xandercruz2k@gmail.com>
2025-07-23 20:57:27 -0400

    feat(main): Add Invoice routes

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from src.modules.auth.infrastructure.auth_fastapi_router import  auth_routes
 from src.modules.category.infrastructure.category_fastapi_router import categories_routes
 from src.modules.client.infrastructure.client_fastapi_router import  client_routes
+from src.modules.invoice.infrastructure.invoice_fastapi_router import invoice_routes
 from src.modules.service.infrastructure.service_fastapi_router import services_routes
 from src.modules.shop.infrastructure.shop_fastapi_router import shops_routes
 from src.modules.stats_history.infrastructure.stats_history_fastapi_router import stats_routes
@@ -27,5 +28,7 @@ app.include_router(categories_routes)
 app.include_router(stats_routes)
 
 app.include_router(shops_routes)
+
+app.include_router(invoice_routes)
 
 
